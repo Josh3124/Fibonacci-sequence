48763e5b8f764d9bdc63e8c6ed79b6f22907c96d
Joash <55209306+Josh3124@users.noreply.github.com>
2020-06-19 10:19:40 -0700

    Fibonacci.py
    Fibonacci sequence

---
+++ Fibonacci.py
@@ -0,0 +1,9 @@
+# This function will calculate the fibonacci sequence into its numbers, this is python.
+def fib(n):
+  a, b = b + a+b
+  while a < n:
+  print(a, end=" ")
+  a, b = b, a+b
+  print()
+  
+  fib(369)
